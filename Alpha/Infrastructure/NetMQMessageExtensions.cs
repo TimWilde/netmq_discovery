7d71e1440840f3871a9329a5b315500f6bdc0de6
Tim Wilde <tim@timwil.de>
2020-06-13 16:37:57 +0100

    Encapsulate debug logging of message frames.

---
+++ Alpha/Infrastructure/NetMQMessageExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Alpha.Infrastructure
+{
+   using System.Linq;
+   using NetMQ;
+
+   public static class NetMQMessageExtensions
+   {
+      public static string Dump( this NetMQMessage message )
+      {
+         return $"{string.Join( ",", message.Select( frame => frame.IsEmpty ? "<empty>" : frame.ConvertToString() ) )}";
+      }
+   }
+}
